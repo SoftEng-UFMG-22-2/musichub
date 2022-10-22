6688e97a57bb0d1ef8168378ecd5341c9a761c1e
IvanGomesCruz <97919328+IvanGomesCruz@users.noreply.github.com>
2022-10-22 17:53:07 -0300

    Revert "att artists layout"
    This reverts commit 9113bb2a6323d335c21cb1832bd1d21519b787ad.

--- frontend/src/pages/components/Artist.js
+++ frontend/src/pages/components/Artist.js
@@ -3,16 +3,10 @@ import './Artist.css'
 
 function Artist({name, image}) {
   return (
-    <div class = "top_card">
-    <div class = "top_card_number"><h1>1</h1>
+    <div className="artist-box">
+        <img src={image}></img>
+        <h3>{name}</h3>
     </div>
-
-    <div class = "top_card_imagebox">
-        
-        <img src={image}> </img>   
-    </div>
-    <p>{name}</p>
-</div>
   )
 }
 
